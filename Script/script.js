e8a8bf11d60fe94894447916406b8be06fdf49a4
H44K0N <haakon.tischbein@gmail.com>
2025-02-07 16:38:08 +0100

    fixed fullscreen

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    if (!document.fullscreenElement) {
+      if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+      } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+        document.documentElement.mozRequestFullScreen();
+      } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, Opera
+        document.documentElement.webkitRequestFullscreen();
+      } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+        document.documentElement.msRequestFullscreen();
+      }
+    }
+  }, 1000); // Delay for 1 second to ensure user interaction
+});
+
 let wakeLock = null;
 
 async function requestWakeLock() {
